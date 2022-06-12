828387f32d8f7483166d7336e58550a6e1577896
Novrianda HadhiPutra <novriandahp@gmail.com>
2022-06-12 15:09:27 +0700

    test push

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
